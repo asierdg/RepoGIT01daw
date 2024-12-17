9bf2e7d7134ee62b4e687d881a43ccf7490c803b
Asier Díez <asier.diez06@somo.eus>
2024-12-17 10:33:10 +0100

    mensaje

---
+++ src/clasesDatos/Empleado.java
@@ -0,0 +1,11 @@
+package clasesDatos;
+
+public class Empleado {
+	String dni;
+	String nombre;
+	String apellidos;
+	int edad;
+	boolean clubSocial; //indica si el empleado está en el club social de la empresa
+	float sueldo;
+	
+}
